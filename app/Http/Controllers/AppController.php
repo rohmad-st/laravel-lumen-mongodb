b2d31931d0671ba31fa2927e561e89c369cebc60
vagrant <vagrant@homestead>
2016-10-18 11:48:19 +0700

    Initial commit

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,19 @@
+<?php namespace App\Http\Controllers;
+
+class AppController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        //
+    }
+}
